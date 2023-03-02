25580491dd29f8f3f58057dea67b0ca078e32983
EC2 Default User <ec2-user@ip-172-31-21-22.ec2.internal>
2023-03-02 21:37:45 +0000

    lab7.py

---
+++ lab7.py
@@ -0,0 +1,59 @@
+'''
+lab7
+while Loop
+'''
+
+#3.1
+i = 5
+while i>=0:
+    i=i-1
+    if i ==3:
+        continue
+    print(i)
+    
+    
+#3.2
+i = 1
+result =1
+while i<=5:
+    result = result * i
+    i = i +1
+print(result)
+
+#3.3
+i = 1
+result = 0
+
+while i<=5:
+    result=result + i
+    i = i +1
+print(result)
+
+
+#3.4
+i =3
+result = 1
+while i <=8:
+    result = result * i
+    i = i +1
+print(result)
+    
+    
+#3.5
+i =4
+result = 1
+while i<=8:
+    result = result * i
+    i = i +1
+print(result)
+
+#3.6
+num_list = [12,32,43,35]
+while num_list:
+    num_list.pop()
+    print("current list:", num_list)
+
+
+    
+
+
---
+++ lec7.py
@@ -0,0 +1,41 @@
+'''
+lec7
+while loop
+continue, break, and pass
+'''
+
+
+
+#for item in ['a','b']:
+ #   print (item)
+#    i=5
+#    while i >0:
+ #       i = i-1
+  #      print(i)
+ #   
+ #       if i ==3:
+ #           break
+        
+        
+#i = 5
+#while i >0:
+   # i=i-1
+    
+   # if i ==3:
+       # continue
+    
+    #print (i)
+    
+    
+i=5
+while i>=0:
+    i=i-1
+    if i ==3:
+        pass
+    print (i)
+    
+try:
+    print(1/0)
+
+except:
+    print('handle error2')
